bc79f03560cec86ff25ac3a97f0613ea8b627997
zhengjunxiong <415268015@qq.com>
2021-09-30 20:51:51 +0800

    开工

---
+++ go.mod
@@ -0,0 +1,9 @@
+module open-devops
+
+go 1.16
+
+require (
+	github.com/alecthomas/template v0.0.0-20190718012654-fb15b899a751 // indirect
+	github.com/alecthomas/units v0.0.0-20210927113745-59d0afb8317a // indirect
+	gopkg.in/alecthomas/kingpin.v2 v2.2.6
+)
---
+++ src/modules/server/server.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"gopkg.in/alecthomas/kingpin.v2"
+)
+
+var (
+	verbose = kingpin.Flag("verbose", "Verbose mode.").Short('v').Bool()
+	name    = kingpin.Arg("name", "Name of user.").Required().String()
+)
+
+func main() {
+	kingpin.Parse()
+	fmt.Printf("%v, %s\n", *verbose, *name)
+}
